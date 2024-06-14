829dee78cd4ab5de00c48b5f4b06cec87e08830a
Omkar Chandrakant Kshirsagar <ookkshirsagar@gmail.com>
2024-06-14 06:52:50 +0200

    ToF_Test

---
+++ examples/ToF_Test.py
@@ -0,0 +1,16 @@
+import time
+import board
+import busio
+import adafruit_vl53l0x
+
+# Initialize I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialize the ToF sensor
+sensor = adafruit_vl53l0x.VL53L0X(i2c)
+
+while True:
+    # Read the distance in millimeters
+    distance = sensor.range
+    print("Distance: {} mm".format(distance))
+    time.sleep(1)

21f47cf9fa9707626ea39303287cde6395a96009
ookkshirsagar <ookkshirsagar@gmail.com>
2024-07-01 15:38:40 +0200

    Sevo Test

--- examples/Test_Servo.py
+++ examples/Test_Servo.py
@@ -22,10 +22,10 @@ pwm_2.start(0)
 def set_servo_angle(pwm, angle):
     duty_cycle = 2.5 + (angle / 18.0)  # Adjust the duty cycle calculation if needed
     pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(0.5)  # Give time for the servo to reach the position
+    
 
 # Initial angles
-initial_angle_1 = 180
+initial_angle_1 = 170
 initial_angle_2 = 0
 
 try:
@@ -36,12 +36,12 @@ try:
 
     while True:
         # Move the first servo from 180 to 160 degrees and the second from 0 to 20 degrees
-        set_servo_angle(pwm_1, 160)
-        set_servo_angle(pwm_2, 20)
+        set_servo_angle(pwm_1, 140)
+        set_servo_angle(pwm_2, 30)
         time.sleep(0.5)  # Adjust delay as necessary for servos to reach position
 
         # Move the first servo from 160 to 180 degrees and the second from 20 to 0 degrees
-        set_servo_angle(pwm_1, 180)
+        set_servo_angle(pwm_1, 170)
         set_servo_angle(pwm_2, 0)
         time.sleep(0.5)  # Adjust delay as necessary for servos to reach position
 

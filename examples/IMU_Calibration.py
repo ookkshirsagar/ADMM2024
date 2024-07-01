d5a911a87f7bf7c7a013f84f2215d7727509e580
Omkar Chandrakant Kshirsagar <ookkshirsagar@gmail.com>
2024-07-01 21:56:53 +0200

    IMU Calibration

--- examples/IMU_Calibration.py
+++ examples/IMU_Calibration.py
@@ -1,22 +1,73 @@
+import time
 from mpu6050 import mpu6050
-from time import sleep
 
-sensor = mpu6050(0x68)
+# MPU6050 sensor address
+sensor_address = 0x68
+sensor = mpu6050(sensor_address)
 
-while True:
-    accel_data = sensor.get_accel_data()
+# Number of samples to collect for calibration
+calibration_samples = 1000
+
+def calibrate_mpu6050(sensor, calibration_samples):
+    print("Calibrating MPU6050... Keep the sensor still!")
+    
+    gyro_offsets = [0.0, 0.0, 0.0]
+    accel_offsets = [0.0, 0.0, 0.0]
+    
+    for i in range(calibration_samples):
+        gyro_data = sensor.get_gyro_data()
+        accel_data = sensor.get_accel_data()
+        
+        gyro_offsets[0] += gyro_data['x']
+        gyro_offsets[1] += gyro_data['y']
+        gyro_offsets[2] += gyro_data['z']
+        
+        accel_offsets[0] += accel_data['x']
+        accel_offsets[1] += accel_data['y']
+        accel_offsets[2] += accel_data['z']
+        
+        time.sleep(0.01)  # Adjust sample rate as necessary
+    
+    gyro_offsets = [offset / calibration_samples for offset in gyro_offsets]
+    accel_offsets = [offset / calibration_samples for offset in accel_offsets]
+    
+    print(f"Gyro offsets: {gyro_offsets}")
+    print(f"Accel offsets: {accel_offsets}")
+    
+    return gyro_offsets, accel_offsets
+
+def get_corrected_gyro_data(sensor, gyro_offsets):
     gyro_data = sensor.get_gyro_data()
-    temp = sensor.get_temp()
+    corrected_gyro = {
+        'x': gyro_data['x'] - gyro_offsets[0],
+        'y': gyro_data['y'] - gyro_offsets[1],
+        'z': gyro_data['z'] - gyro_offsets[2]
+    }
+    return corrected_gyro
 
-    print("Accelerometer data")
-    print("x: " + str(accel_data['x']))
-    print("y: " + str(accel_data['y']))
-    print("z: " + str(accel_data['z']))
+def get_corrected_accel_data(sensor, accel_offsets):
+    accel_data = sensor.get_accel_data()
+    corrected_accel = {
+        'x': accel_data['x'] - accel_offsets[0],
+        'y': accel_data['y'] - accel_offsets[1],
+        'z': accel_data['z'] - accel_offsets[2]
+    }
+    return corrected_accel
 
-    print("Gyroscope data")
-    print("x: " + str(gyro_data['x']))
-    print("y: " + str(gyro_data['y']))
-    print("z: " + str(gyro_data['z']))
-
-    print("Temp: " + str(temp) + " C")
-    sleep(0.5)
+# Main function to run calibration
+if __name__ == "__main__":
+    try:
+        gyro_offsets, accel_offsets = calibrate_mpu6050(sensor, calibration_samples)
+        
+        print("\nCalibration complete. Now reading corrected values:")
+        while True:
+            corrected_gyro = get_corrected_gyro_data(sensor, gyro_offsets)
+            corrected_accel = get_corrected_accel_data(sensor, accel_offsets)
+            
+            print(f"Corrected Gyro: {corrected_gyro}")
+            print(f"Corrected Accel: {corrected_accel}")
+            
+            time.sleep(0.5)
+    
+    except KeyboardInterrupt:
+        print("\nExiting program.")

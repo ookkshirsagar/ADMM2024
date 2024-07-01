67444a6d8b9230e48b64c3f1e30c347afeeb9938
Omkar Chandrakant Kshirsagar <ookkshirsagar@gmail.com>
2024-07-01 15:20:33 +0200

    sero

--- examples/Test_Servo.py
+++ examples/Test_Servo.py
@@ -22,7 +22,7 @@ pwm_2.start(0)
 def set_servo_angle(pwm, angle):
     duty_cycle = 2.5 + (angle / 18.0)  # Adjust the duty cycle calculation if needed
     pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(0.3)  # Give time for the servo to reach the position
+    time.sleep(0.5)  # Give time for the servo to reach the position
 
 # Initial angles
 initial_angle_1 = 180
@@ -36,16 +36,14 @@ try:
 
     while True:
         # Move the first servo from 180 to 160 degrees and the second from 0 to 20 degrees
-        for angle_1, angle_2 in zip(range(180, 159, -20), range(0, 21, 20)):
-            set_servo_angle(pwm_1, angle_1)
-            set_servo_angle(pwm_2, angle_2)
-            time.sleep(0.3)  # Adjust delay as necessary for servos to reach position
+        set_servo_angle(pwm_1, 160)
+        set_servo_angle(pwm_2, 20)
+        time.sleep(0.5)  # Adjust delay as necessary for servos to reach position
 
         # Move the first servo from 160 to 180 degrees and the second from 20 to 0 degrees
-        for angle_1, angle_2 in zip(range(160, 181, 20), range(20, -1, -20)):
-            set_servo_angle(pwm_1, angle_1)
-            set_servo_angle(pwm_2, angle_2)
-            time.sleep(0.3)  # Adjust delay as necessary for servos to reach position
+        set_servo_angle(pwm_1, 180)
+        set_servo_angle(pwm_2, 0)
+        time.sleep(0.5)  # Adjust delay as necessary for servos to reach position
 
 except KeyboardInterrupt:
     print("Program stopped by user")
